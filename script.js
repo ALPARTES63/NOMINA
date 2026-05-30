6824fbcd0c754919b334c07d33fcb4ca73250a5f
Alpartes <alpartes6@gmail.com>
2026-05-30 11:43:01 -0500

    actualizacion

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ function buscarEmpleado(){
         "resultado"
     ).innerHTML = `
 
-        <h2>${empleado.NOMBRE}</h2>
+        <h2 class="nombre-empleado">${empleado["NOMBRE"]}</h2>
 
         <hr>
 
